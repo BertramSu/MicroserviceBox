cc3600edc1802311e0769a5e47d49aef41c060a3
BertramSu <40878105+BertramSu@users.noreply.github.com>
2024-04-17 22:47:01 -0500

    Switch to gradle

--- Box/src/main/java/com/bert/Box/BoxApplication.java
+++ Box/src/main/java/com/bert/Box/BoxApplication.java
@@ -1,4 +1,4 @@
-package com.bertram.box;
+package com.bert.Box;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Box/src/test/java/com/bert/Box/BoxApplicationTests.java
+++ Box/src/test/java/com/bert/Box/BoxApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bertram.box;
+package com.bert.Box;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
